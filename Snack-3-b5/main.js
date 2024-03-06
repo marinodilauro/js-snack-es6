1c86c7b53118bdac7167d1e2b1d5db2da72aa30b
marinodilauro <m.dilauro90@gmail.com>
2024-03-06 12:49:17 +0100

    Add snack-3-b5

--- Snack-3-b5/main.js
+++ Snack-3-b5/main.js
@@ -1,33 +1,21 @@
 /*
-Crea 10 oggetti che rappresentano una zucchina. Dividi in due array separati le zucchine che misurano meno o più di 15cm.
-Infine stampa separatamente quanto pesano i due gruppi di zucchine.
+Scrivi una funzione che accetti una stringa come argomento e la ritorni girata (es. Ciao -> oaiC)
 */
 
-const zucchine = [
-  { type: 'zucchino nero di Milano', weight: 70, length: 21 },
-  { type: 'zucchino romanesco', weight: 80, length: 12 },
-  { type: 'zucchino bianco di Trieste', weight: 80, length: 16 },
-  { type: 'zucchino striato', weight: 75, length: 22 },
-  { type: 'zucchino giallo', weight: 100, length: 23 },
-  { type: 'zucchino da fiore', weight: 70, length: 14 },
-  { type: 'zucchino ad alberello', weight: 65, length: 22 },
-  { type: 'zucchino tondo', weight: 75, length: 13 },
-  { type: 'zucchino trombetta di Albenga', weight: 110, length: 20 },
-  { type: 'zucchino patisson', weight: 100, length: 11 },
-];
 
-const zucchineSmall = zucchine.filter(zucchina => zucchina.length < 15);
-const zucchineLarge = zucchine.filter(zucchina => zucchina.length >= 15);
+function reverseWord(word) {
 
+  console.log(word);
 
-let zucchineSmallTotalWeight = 0;
-zucchineSmall.forEach(zucchina => zucchineSmallTotalWeight += zucchina.peso);
+  const userWordArray = Array.from(word)
 
-let zucchineLargeTotalWeight = 0;
-zucchineLarge.forEach(zucchina => zucchineLargeTotalWeight += zucchina.peso);
+  const userWordArrayReversed = userWordArray.reverse();
 
-console.log(zucchineSmall);
-console.log(`Il peso totale delle zucchine corte è ${zucchineSmallTotalWeight}g`);
+  const userWordReversed = userWordArrayReversed.join("");
 
-console.log(zucchineLarge);
-console.log(`Il peso totale delle zucchine lunghr è ${zucchineLargeTotalWeight}g`);
+  return userWordReversed
+};
+
+const reversedWord = reverseWord("Ciao");
+
+console.log(reversedWord);
